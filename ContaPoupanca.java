4c88c638429b79c4708f82addb527e8f5a27e7c8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-02 13:00:24 -0300

    Criação da classe com seus métodos da herança.

---
+++ ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca extends ContaBancaria {
+    @Override
+    public void consultarSaldo() {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+}
