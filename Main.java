5aedfdd1bd1544ae9e94d043c759d0fd794f605b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-02 13:39:36 -0300

    Método para exibir menu principal.

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public void exibeMenu() {
+        System.out.println("----------|MENU|----------");
+        System.out.println("1 - Consultar Saldo da Conta Corrente");
+        System.out.println("2 - Depositar na Conta Corrente");
+        System.out.println("3 - Consultar Saldo da Conta Poupança");
+        System.out.println("4 - Depositar na Conta Poupança");
+        System.out.println("5 - Sair");
+    }
 }
